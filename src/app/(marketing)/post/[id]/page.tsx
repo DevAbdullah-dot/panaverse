48bb418c388d1e3724e4b677e186410489006074
Abdullah Siraj <125889514+DevAbdullah-dot@users.noreply.github.com>
2024-02-24 13:53:20 +0500

    commit

--- src/app/(marketing)/post/[id]/page.tsx
+++ src/app/(marketing)/post/[id]/page.tsx
@@ -1,24 +1,19 @@
-import { postdata } from "@/app/data/postdata"
+import { postdata } from "@/app/data/postdata";
 
-async function getPostdata(id: string){
-    const post = postdata.find(post => post.id === id)
-    return post
+async function getPostdata(id: string) {
+  const post = postdata.find((post) => post.id === id);
+  return post;
 }
 
-export async function getStaticParams(){
-    const ids: string[] = ['1','2']
-    return ids.map((id) => ({id:id}))
-}
-
-export default async function postwithId({params}: {params: {id: string}}){
-    const {id} = params;
-    const post = await getPostdata(id);
-    return (
-        <>
-        <div className="flex flex-col items-center justify-between p-24">
-            <h1>{post?.title}</h1>
-            <h1>{post?.date}</h1>
-        </div>
-        </>
-    )
+export default async function postwithId({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const post = await getPostdata(id)
+  return (
+    <>
+      <div className="flex flex-col items-center justify-between p-24">
+        <h1>{post!.title}</h1>
+        <h1>{post!.date}</h1>
+      </div>
+    </>
+  );
 }
